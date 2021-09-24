78ec34a817d3e9d5f9a6ffcdf0f92eb5e5fb9e2c
uiinterpreter <premp4920@gmail.com>
2021-09-24 09:48:39 +0200

    First commit for project creation.

---
+++ pages/video.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-row>
+    <v-col class="text-center">
+      <img
+        src="/v.png"
+        alt="Vuetify.js"
+        class="mb-5"
+      >
+
+    </v-col>
+  </v-row>
+</template>
